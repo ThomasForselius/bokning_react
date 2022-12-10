ffc2599fc725c84bddb4fdf764843204335e47c8
Thomas Forselius <forselius.thomas@gmail.com>
2022-12-10 12:41:10 +0100

    styled navbar and reinstalled package.json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import styles from './App.module.css';
 import NavBar from './Components/NavBar';
+import { Route, Switch } from "react-router-dom"
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Homepage</h1>} />
+          <Route exact path="/check" render={() => <h1>Check room</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,18 +2,30 @@ import Container from 'react-bootstrap/Container';
 import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import styles from '../styles/NavBar.module.css'
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="sm" fixed="top">
+    <Navbar className={styles.NavBar} expand="sm" fixed="top">
     <Container>
-      <Navbar.Brand href="#home">Bokning</Navbar.Brand>
+      <NavLink to='/' exact>
+        <Navbar.Brand>
+          Bokning
+        </Navbar.Brand>
+      </NavLink>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto text-left">
-            <Nav.Link href="#link">Check rooms</Nav.Link>
-            <Nav.Link href="#home">Sign in <i className="fa-solid fa-right-to-bracket"></i></Nav.Link>
-            <Nav.Link>Register <i className="fa-solid fa-user-plus"></i></Nav.Link>
+          <NavLink to='/check' className={styles.NavLink} activeClassName={styles.Active}>  
+              Check availability<i className='fa-solid fa-house'></i>
+          </NavLink>
+          <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
+              Sign in <i className="fa-solid fa-right-to-bracket"></i>
+          </NavLink>
+          <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
+              Register <i className="fa-solid fa-user-plus"></i>
+          </NavLink>
         </Nav>
       </Navbar.Collapse>
     </Container>
