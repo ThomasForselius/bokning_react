a9813cbd65dbe139ca68aabccc880377292237ab
Thomas Forselius <forselius.thomas@gmail.com>
2022-12-11 20:23:48 +0100

    fixed axios get in signinform

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -27,7 +27,7 @@ const SignInForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("/dj-rest-auth/login/", signInData);
+      const {data} = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user)
       history.push('/check');
     } catch (error){
