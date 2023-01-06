c786264643e24e20a2dfbcfdd763d3f550a4fa18
Thomas Forselius <forselius.thomas@gmail.com>
2023-01-06 20:03:59 +0100

    created book function to booking page

--- src/pages/posts/BookingCreateForm.js
+++ src/pages/posts/BookingCreateForm.js
@@ -2,18 +2,24 @@ import React, { useContext, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
+import { Alert } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import styles from "../../styles/BookingCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import axios from "axios";
+import { CurrentUserContext } from '../../context/CurrentUserContext'
+import { axiosReq } from "../../api/axiosDefaults";
+import { useHistory } from "react-router-dom";
 
 function BookingCreateForm() {
   const [errors, setErrors] = useState({});
+  const currentUser = useContext(CurrentUserContext);
+  const history = useHistory();
   const [bookingData, setBookingData] = useState({
     date: '',
     username: '',
+    desc: '',
   });
 
   const handleChange = (event) => {
@@ -23,34 +29,55 @@ function BookingCreateForm() {
     })
     console.log(event.target.value);
   };
-
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const formData = new FormData();
+    formData.append('date', date)
+    formData.append('username', currentUser)
+    formData.append('desc', desc)
+    
     try{
-      const {data} = await axios.post("/dj-rest-auth/bookings/", bookingData)
+      const {data} = await axiosReq.post("/dj-rest-auth/bookings/", formData);
+      history.push('/posts/book/${data.id}')
     } catch (error){
-      setErrors(error.response?.data)
+      console.log(error)
+      if(error.response?.status !== 401){
+        setErrors(error.response?.data)
+      }
     }
   };
+
+  const {date, desc} = bookingData;
 
   const textFields = (
     <div className="text-center">  
       <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-5" controlId="date">
+        <Form.Group className="mb-0" controlId="date">
           <Form.Label className={`${styles.label} "d-sm"`}>Date</Form.Label>
           <Form.Control 
             className={styles.input}
             type="date"
             name="date"
-            value={bookingData.date}
+            required
+            value={date}
             onChange={handleChange}
             />
       
-          <Form.Control className={styles.input} as="textarea" rows={2} name="desc" placeholder="Comment" />
+          <Form.Control 
+            className={styles.input} 
+            as="textarea" 
+            rows={2} 
+            name="desc" 
+            placeholder="Comment"
+            value={desc}
+            onChange={handleChange} 
+            />
         </Form.Group>
-
+        {errors.non_field_errors?.map((message, idx) => (
+              <Alert variant="warning" className={styles.alert} key={idx}>{message}</Alert>
+              ))}
           <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-            Book
+            Book 
           </Button>
 
       </Form>
@@ -60,23 +87,6 @@ function BookingCreateForm() {
   return (
     <Form>
       <Row>
-        {/* <Col className="p-2 p-sm-2" md={7} lg={8}>
-          <Container
-            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
-          >
-            <Form.Group className="text-center">
-              
-                <Form.Label
-                  className="d-flex justify-content-center"
-                  htmlFor="image-upload"
-                > Image upload
-                </Form.Label>
-
-            </Form.Group>
-            <div className="d-md-none">{textFields}</div>
-          </Container>
-        </Col> */}
-
         <Col>
           <Container className={appStyles.Content}>{textFields}</Container>
         </Col>
