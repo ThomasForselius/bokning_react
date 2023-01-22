3bd87023ccb107539661b71fda5b1ab24c816e3e
Thomas Forselius <forselius.thomas@gmail.com>
2023-01-22 13:18:26 +0100

    fixed filter function on booking list

--- src/pages/booking/BookingList.js
+++ src/pages/booking/BookingList.js
@@ -12,7 +12,11 @@ function BookingList({ filter, message }) {
     const [booking, setBooking] = useState({results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
 
-    
+    const handleQueryChange = (event) => {
+        setQuery(event.target.value)
+        setHasLoaded(false)
+      };
+
     useEffect(() => { 
         const fetchBookings = async () => {
             try{
@@ -24,22 +28,7 @@ function BookingList({ filter, message }) {
             }
         }
         fetchBookings()
-    },[filter])
-    
-    const handleQueryChange = (event) => {
-        setQuery(event.target.value)
-        console.log(event.target.value)
-        setHasLoaded(false)
-        const filteredBookings = booking.results.includes(event.target.value)
-        console.log(filteredBookings)
-        // const filteredBookings = booking.results.filter(book => {
-            //return book.includes(query)
-        //})
-        setBooking({
-            results: filteredBookings
-        })
-        setHasLoaded(true)
-      };
+    },[filter, query])
 
     return (
     <Row className="h-100">
